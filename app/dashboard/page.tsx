65ccec900c92ffc8c2edefeb05c8927cbe8cd572
Abdul Mahl <mdu_abdul@icloud.com>
2024-05-04 16:36:00 +0200

    .

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,14 @@
-export default function Dashboard() {
+import { lazy } from 'react';
+const Card = lazy(() => import('@/app/ui/dashboard/cards'));
+const RevenueChart = lazy(() => import('@/app/ui/dashboard/revenue-chart'));
+const Latestinvoices = lazy(() => import('@/app/ui/dashboard/latest-invoices'));
+import { lusitana } from '../ui/fonts';
+
+export default async function Dashboard() {
   return (
-    <p>
-      Dashboard Page
-    </p>
+    <main>
+      <h1 className={`${lusitana.className}`}></h1>
+    </main>
   )
+
 }
