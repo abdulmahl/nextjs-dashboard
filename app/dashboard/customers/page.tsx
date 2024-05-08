db8cd66af63cdac787aca50d561caa1553d784b1
Abdul Mahl <mdu_abdul@icloud.com>
2024-05-08 23:05:11 +0200

    .

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,17 @@
 import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
+
+
+
 
 export const metadata: Metadata = {
   title: 'Customers | Acme Dashboard',
 };
 
 export default function Customers() {
-  return <p>Customers Page</p>;
+  return (
+    <p className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      Customers Page
+    </p>
+  );
 }
