902c993126d99a224b2028f481dc150123cb8af8
Abdul Mahl <mdu_abdul@icloud.com>
2024-06-24 17:02:48 +0200

    added a static register form.

---
+++ app/register/page.tsx
@@ -0,0 +1,24 @@
+import AcmeLogo from '../ui/acme-logo';
+import RegisterForm from '../ui/register-form';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme | Register',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+export default function RegisterPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <RegisterForm />
+      </div>
+    </main>
+  );
+}
