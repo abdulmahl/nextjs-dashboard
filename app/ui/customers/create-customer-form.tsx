745432663af0391f7946201e10238b42e9dbd80e
Abdul Mahl <mdu_abdul@icloud.com>
2024-06-25 11:48:41 +0200

    Done with creating the create user form.

--- app/ui/customers/create-customer-form.tsx
+++ app/ui/customers/create-customer-form.tsx
@@ -1,10 +1,58 @@
 import React from 'react';
 import Link from 'next/link';
 import { Button } from '../button';
+import { AtSymbolIcon, KeyIcon, UserIcon } from '@heroicons/react/24/outline';
 
 export default function CustomerForm() {
   return (
     <form>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        <label htmlFor="name">Name</label>
+        <div className="relative mt-2 rounded-md">
+          <div className="relative">
+            <input
+              id="name"
+              name="name"
+              type="text"
+              step="0.01"
+              placeholder="Enter your name"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              required
+            />
+            <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          </div>
+        </div>
+        <label htmlFor="email">Email</label>
+        <div className="relative mt-2 rounded-md">
+          <div className="relative">
+            <input
+              id="email"
+              name="email"
+              type="text"
+              step="0.01"
+              placeholder="Enter your email"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              required
+            />
+            <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          </div>
+        </div>
+        <label htmlFor="name">Password</label>
+        <div className="relative mt-2 rounded-md">
+          <div className="relative">
+            <input
+              id="name"
+              name="name"
+              type="text"
+              step="0.01"
+              placeholder="Enter your name"
+              className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              required
+            />
+            <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          </div>
+        </div>
+      </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/customers"
