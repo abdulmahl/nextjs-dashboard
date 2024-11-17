72c723f978b9fda8c03f4c86205e06bc26b19c00
Abdul Mahl <mdu_abdul@icloud.com>
2024-11-17 09:22:20 +0200

    .

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,6 +3,7 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import InvoicedLogo from '@/app/ui/invoiced-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
+import { redirect } from 'next/navigation';
 
 export default function SideNav() {
   return (
@@ -19,7 +20,8 @@ export default function SideNav() {
         <form
           action={async () => {
             'use server';
-            await signOut();
+            await signOut({ redirect: false });
+            redirect('https://nextjs-dashboard-two-xi-91.vercel.app');
           }}
         >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
